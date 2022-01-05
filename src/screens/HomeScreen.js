bf8d1bfeb9c8be6b5e533c6645d8eeedcf8def34
aimee-annabelle <inezannabelle@gmail.com>
2022-01-05 08:40:00 +0200

    searchbar bug

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import {View, Text,StyleSheet} from 'react-native';
-import SearchBar from "react-native-dynamic-search-bar";
+// import SearchBar from "react-native-dynamic-search-bar";
+import SearchBarComponent from '../components/searchBarComponent';
 
 const HomeScreen = () =>{
     return (
         <View>
-            <SearchBar
+        {/* <SearchBar
             style={styles.searchStyle}
             placeholder="Search here"
-            onChangeText={(text) => this.filterList(text)}
-            onSearchPress={() => console.log("Search Icon is pressed")}
-            onClearPress={() => this.filterList("")}
             onPress={() => alert("onPress")}
-            />
-            <Text>Welcome to enCake,</Text>
-            <Text>What dessert would you like!</Text>
+            onChangeText={(text) => console.log(text)}
+        /> */}
+        <SearchBarComponent/>
+        <Text>Welcome to enCake,</Text>
+        <Text>What dessert would you like!</Text>
         </View>
         )
 };
 const styles = StyleSheet.create({
     searchStyle:{
-        fontColor="#c6c6c6",
-        iconColor="#c6c6c6",
-        shadowColor="#282828",
-        cancelIconColor="#c6c6c6",
-        backgroundColor="#353d5e"
+        color:'#c6c6c6',
+        backgroundColor:'#ffffff'
     }
-
 });
 
 export default HomeScreen;
